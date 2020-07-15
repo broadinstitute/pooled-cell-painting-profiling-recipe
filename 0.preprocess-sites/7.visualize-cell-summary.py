fc9771804258289a25fb4f94dff2c048d6009957
ErinWeisbart <eweisbar@broadinstitute.org>
2020-07-15 10:33:19 -0700

    Revert "black"
    This reverts commit 4effd1abd9e5ca782abd1b9e3c07ea1b6298d91a.

--- 0.preprocess-sites/7.visualize-cell-summary.py
+++ 0.preprocess-sites/7.visualize-cell-summary.py
@@ -47,9 +47,7 @@ painting_image_names = summ_plate_args["painting_image_names"]
 barcoding_cycles = summ_plate_args["barcoding_cycles"]
 barcoding_prefix = summ_plate_args["barcoding_prefix"]
 
-cell_count_file = pathlib.Path(
-    summ_cells_results_basedir, batch, "cells", "cell_count.tsv"
-)
+cell_count_file = pathlib.Path(summ_cells_results_basedir, batch, "cells", "cell_count.tsv")
 cell_count_df = pd.read_csv(cell_count_file, sep="\t")
 
 figures_output = pathlib.Path(summ_cells_figures_basedir, batch)
@@ -63,13 +61,13 @@ x_seq = 1
 y_seq = 1
 build_seq = []
 final_order = []
-for i in range(1, ((acquisition_side * acquisition_side) + 1)):
+for i in range(1, ((acquisition_side * acquisition_side) +1)):
     if len(final_order) == (acquisition_side * acquisition_side):
         break
     else:
         build_seq.append([x_seq, y_seq])
         if len(build_seq) == acquisition_side:
-            x_seq = x_seq + 1
+            x_seq = x_seq +1
         if y_seq == acquisition_side:
             y_seq = 0
             build_seq = build_seq[::-1]
@@ -79,81 +77,60 @@ for i in range(1, ((acquisition_side * acquisition_side) + 1)):
 sites_list = [*range(1, (acquisition_side * acquisition_side) + 1)]
 
 # uses sites_list in case there are fewer analyzed sites than acquired sites
-loc_df = pd.DataFrame(final_order).rename(columns={0: "x_loc", 1: "y_loc"})
+loc_df = pd.DataFrame(final_order).rename(columns={0:'x_loc', 1:'y_loc'})
 loc_df["Site"] = sites_list
 
 # create total_cell_count
-cell_count_bysite_df = (
-    cell_count_df.groupby("site")["cell_count"]
-    .sum()
-    .reset_index()
-    .rename(columns={"cell_count": "total_cell_count"})
-)
+cell_count_bysite_df = cell_count_df.groupby("site")["cell_count"].sum().reset_index().rename(columns={'cell_count':'total_cell_count'})
 # add total_cell_count to cell_count_df
 cell_count_df = cell_count_df.merge(cell_count_bysite_df, on="site")
-# add in x, y coordinates for plotting
+#add in x, y coordinates for plotting
 cell_count_df = cell_count_df.merge(loc_df, left_on="Site", right_on="Site")
 
 # Plot total number of cells per well
-cell_count_totalcells_df = (
-    cell_count_df.groupby(["x_loc", "y_loc", "Well", "Site"])["total_cell_count"]
-    .mean()
-    .reset_index()
-)
+cell_count_totalcells_df = cell_count_df.groupby(["x_loc", "y_loc", "Well", "Site"])["total_cell_count"].mean().reset_index()
 os.makedirs(output_figuresdir, exist_ok=True)
 
 by_well_gg = (
-    gg.ggplot(cell_count_totalcells_df, gg.aes(x="x_loc", y="y_loc"))
-    + gg.geom_point(gg.aes(fill="total_cell_count"), size=10)
-    + gg.geom_text(gg.aes(label="Site"), color="lightgrey")
-    + gg.facet_wrap("~Well")
-    + gg.coord_fixed()
-    + gg.theme_bw()
-    + gg.theme(
-        axis_text=gg.element_blank(),
-        axis_title=gg.element_blank(),
-        strip_background=gg.element_rect(colour="black", fill="#fdfff4"),
-    )
-    + gg.scale_fill_cmap(name="magma")
+    gg.ggplot(cell_count_totalcells_df, gg.aes(x="x_loc", y="y_loc")) +
+    gg.geom_point(gg.aes(fill="total_cell_count"), size=10) +
+    gg.geom_text(gg.aes(label="Site"), color="lightgrey") +
+    gg.facet_wrap("~Well") +
+    gg.coord_fixed() +
+    gg.theme_bw() +
+    gg.theme(axis_text=gg.element_blank(),
+             axis_title=gg.element_blank(),
+             strip_background=gg.element_rect(colour="black", fill="#fdfff4")) +
+    gg.scale_fill_cmap(name="magma")
 )
 output_file = pathlib.Path(output_figuresdir, "plate_layout_cells_count_per_well.png")
 by_well_gg.save(output_file, dpi=300, verbose=False)
 
 # Plot cell category ratios per well
-ratio_df = pd.pivot_table(
-    cell_count_df,
-    values="cell_count",
-    index=["site", "Plate", "Well", "Site", "x_loc", "y_loc"],
-    columns=["Cell_Quality"],
-)
+ratio_df = pd.pivot_table(cell_count_df, values='cell_count', index=['site', 'Plate', 'Well', 'Site', 'x_loc', 'y_loc'], columns=['Cell_Quality'])
 ratio_df["Sum"] = ratio_df.sum(axis=1)
 ratio_df["Pass_Filter"] = ratio_df[cell_filter].sum(axis=1)
 fail_filter = [cat for cat in cell_category_order if cat not in cell_filter]
 ratio_df["Fail_Filter"] = ratio_df[fail_filter].sum(axis=1)
 fail_filter_noempty = fail_filter.remove("Empty")
 ratio_df["Fail_Filter_noempty"] = ratio_df[fail_filter].sum(axis=1)
-ratio_df["Pass/Fail_withempty"] = ratio_df["Pass_Filter"] / ratio_df["Fail_Filter"]
-ratio_df["Pass/Fail_0empty"] = ratio_df["Pass_Filter"] / ratio_df["Fail_Filter_noempty"]
-ratio_df["Percent_Empty"] = ratio_df["Empty"] / ratio_df["Sum"]
-ratio_df = ratio_df.drop(
-    cell_category_order + ["Sum", "Pass_Filter", "Fail_Filter", "Fail_Filter_noempty"],
-    1,
-)
-ratio_df = ratio_df.stack().to_frame().reset_index().rename(columns={0: "value"})
+ratio_df["Pass/Fail_withempty"] = ratio_df["Pass_Filter"]/ratio_df["Fail_Filter"]
+ratio_df["Pass/Fail_0empty"] = ratio_df["Pass_Filter"]/ratio_df["Fail_Filter_noempty"]
+ratio_df["Percent_Empty"] = ratio_df["Empty"]/ratio_df["Sum"]
+ratio_df = ratio_df.drop(cell_category_order + ["Sum", "Pass_Filter", "Fail_Filter", "Fail_Filter_noempty"], 1)
+ratio_df = ratio_df.stack().to_frame().reset_index().rename(columns = {0:"value"})
 
 ratio_gg = (
-    gg.ggplot(ratio_df, gg.aes(x="x_loc", y="y_loc"))
-    + gg.geom_point(gg.aes(fill="value"), size=10)
-    + gg.geom_text(gg.aes(label="Site"), color="lightgrey")
-    + gg.facet_grid("Cell_Quality~Well", scales="free_y")
-    + gg.coord_fixed()
-    + gg.theme_bw()
-    + gg.theme(
-        axis_text=gg.element_blank(),
-        axis_title=gg.element_blank(),
-        strip_background=gg.element_rect(colour="black", fill="#fdfff4"),
-    )
-    + gg.scale_fill_cmap(name="magma")
+    gg.ggplot(ratio_df, gg.aes(x="x_loc", y="y_loc")) +
+    gg.geom_point(gg.aes(fill="value"), size=10) +
+    gg.geom_text(gg.aes(label="Site"), color="lightgrey") +
+    gg.facet_grid("Cell_Quality~Well", scales = "free_y") +
+    gg.coord_fixed() +
+    gg.theme_bw() +
+    gg.theme(axis_text=gg.element_blank(),
+             axis_title=gg.element_blank(),
+             strip_background=gg.element_rect(colour="black", fill="#fdfff4")) +
+    gg.scale_fill_cmap(name="magma")
 )
 
 output_file = pathlib.Path(output_figuresdir, "plate_layout_ratios_per_well.png")
@@ -183,82 +160,64 @@ for site in sites:
 image_df = pd.concat(image_list, axis="rows").reset_index(drop=True)
 print("Done concatenating image files")
 
-# add in x, y coordinates for plotting
-image_df["Site"] = image_df["Site"].astype(int)
-loc_df["Site"] = loc_df["Site"].astype(int)
+#add in x, y coordinates for plotting
+image_df['Site']=image_df['Site'].astype(int)
+loc_df['Site']=loc_df['Site'].astype(int)
 image_df = image_df.merge(loc_df, how="left", on="Site")
 
 # Plot final Cells thresholds per well
 cells_finalthresh_gg = (
-    gg.ggplot(image_df, gg.aes(x="x_loc", y="y_loc"))
-    + gg.geom_point(gg.aes(fill="Threshold_FinalThreshold_Cells"), size=10)
-    + gg.geom_text(gg.aes(label="Site"), color="lightgrey")
-    + gg.facet_wrap("~Well")
-    + gg.coord_fixed()
-    + gg.theme_bw()
-    + gg.theme(
-        axis_text=gg.element_blank(),
-        axis_title=gg.element_blank(),
-        strip_background=gg.element_rect(colour="black", fill="#fdfff4"),
-    )
-    + gg.scale_fill_cmap(name="magma")
+gg.ggplot(image_df, gg.aes(x="x_loc", y="y_loc")) +
+gg.geom_point(gg.aes(fill="Threshold_FinalThreshold_Cells"), size=10) +
+gg.geom_text(gg.aes(label="Site"), color="lightgrey") +
+gg.facet_wrap("~Well") +
+gg.coord_fixed() +
+gg.theme_bw() +
+gg.theme(axis_text=gg.element_blank(),
+         axis_title=gg.element_blank(),
+         strip_background=gg.element_rect(colour="black", fill="#fdfff4")) +
+gg.scale_fill_cmap(name="magma")
 )
-output_file = pathlib.Path(
-    output_figuresdir, "plate_layout_Cells_FinalThreshold_per_well.png"
-)
+output_file = pathlib.Path(output_figuresdir, "plate_layout_Cells_FinalThreshold_per_well.png")
 cells_finalthresh_gg.save(output_file, dpi=300, verbose=False)
 
 # Plot final Nuclei thresholds per well
 nuclei_finalthresh_gg = (
-    gg.ggplot(image_df, gg.aes(x="x_loc", y="y_loc"))
-    + gg.geom_point(gg.aes(fill="Threshold_FinalThreshold_Nuclei"), size=10)
-    + gg.geom_text(gg.aes(label="Site"), color="lightgrey")
-    + gg.facet_wrap("~Well")
-    + gg.coord_fixed()
-    + gg.theme_bw()
-    + gg.theme(
-        axis_text=gg.element_blank(),
-        axis_title=gg.element_blank(),
-        strip_background=gg.element_rect(colour="black", fill="#fdfff4"),
-    )
-    + gg.scale_fill_cmap(name="magma")
+gg.ggplot(image_df, gg.aes(x="x_loc", y="y_loc")) +
+gg.geom_point(gg.aes(fill="Threshold_FinalThreshold_Nuclei"), size=10) +
+gg.geom_text(gg.aes(label="Site"), color="lightgrey") +
+gg.facet_wrap("~Well") +
+gg.coord_fixed() +
+gg.theme_bw() +
+gg.theme(axis_text=gg.element_blank(),
+         axis_title=gg.element_blank(),
+         strip_background=gg.element_rect(colour="black", fill="#fdfff4")) +
+gg.scale_fill_cmap(name="magma")
 )
-output_file = pathlib.Path(
-    output_figuresdir, "plate_layout_Nuclei_FinalThreshold_per_well.png"
-)
+output_file = pathlib.Path(output_figuresdir, "plate_layout_Nuclei_FinalThreshold_per_well.png")
 nuclei_finalthresh_gg.save(output_file, dpi=300, verbose=False)
 
 # Plot percent Confluent regions per well
 percent_confluent_gg = (
-    gg.ggplot(image_df, gg.aes(x="x_loc", y="y_loc"))
-    + gg.geom_point(gg.aes(fill="Math_PercentConfluent"), size=10)
-    + gg.geom_text(gg.aes(label="Site"), color="lightgrey")
-    + gg.facet_wrap("~Well")
-    + gg.coord_fixed()
-    + gg.theme_bw()
-    + gg.theme(
-        axis_text=gg.element_blank(),
-        axis_title=gg.element_blank(),
-        strip_background=gg.element_rect(colour="black", fill="#fdfff4"),
-    )
-    + gg.scale_fill_cmap(name="magma")
+gg.ggplot(image_df, gg.aes(x="x_loc", y="y_loc")) +
+gg.geom_point(gg.aes(fill="Math_PercentConfluent"), size=10) +
+gg.geom_text(gg.aes(label="Site"), color="lightgrey") +
+gg.facet_wrap("~Well") +
+gg.coord_fixed() +
+gg.theme_bw() +
+gg.theme(axis_text=gg.element_blank(),
+         axis_title=gg.element_blank(),
+         strip_background=gg.element_rect(colour="black", fill="#fdfff4")) +
+gg.scale_fill_cmap(name="magma")
 )
-output_file = pathlib.Path(
-    output_figuresdir, "plate_layout_PercentConfluent_per_well.png"
-)
+output_file = pathlib.Path(output_figuresdir, "plate_layout_PercentConfluent_per_well.png")
 percent_confluent_gg.save(output_file, dpi=300, verbose=False)
 
 # Create list of sites with confluent regions
 confluent_df = image_df.loc[image_df["Math_PercentConfluent"] > 0]
-confluent_df = (
-    confluent_df[["site", "Plate", "Well", "Site", "Math_PercentConfluent"]]
-    .sort_values(by=["site"])
-    .reset_index(drop=True)
-)
+confluent_df = confluent_df[["site", "Plate", "Well", "Site", "Math_PercentConfluent"]].sort_values(by=["site"]).reset_index(drop=True)
 if len(confluent_df.index) > 0:
-    confluent_output_file = pathlib.Path(
-        results_output, "sites_with_confluent_regions.csv"
-    )
+    confluent_output_file = pathlib.Path(results_output, "sites_with_confluent_regions.csv")
     confluent_df.to_csv(confluent_output_file)
 
 # Power Log Log Slope on Cell Painting images (proxy for focus)
@@ -268,18 +227,16 @@ for name in painting_image_names:
     PLLS_df_cols.append("ImageQuality_PowerLogLogSlope_" + name)
     PLLS_cols.append("ImageQuality_PowerLogLogSlope_" + name)
 PLLS_df = image_df.loc[:, PLLS_df_cols]
-PLLS_df = PLLS_df.melt(id_vars=["Plate", "Well", "Site"], var_name="Channel").replace(
-    {"ImageQuality_PowerLogLogSlope_": ""}, regex=True
-)
+PLLS_df = PLLS_df.melt(id_vars=['Plate', 'Well', 'Site'], var_name='Channel').replace({'ImageQuality_PowerLogLogSlope_':''}, regex=True)
 
 PLLS_gg = (
-    gg.ggplot(PLLS_df, gg.aes(x="Site", y="value", label="Site"))
-    + gg.geom_point()
-    + gg.coord_fixed(ratio=0.25)
-    + gg.geom_text(nudge_y=-0.025)
-    + gg.facet_grid("Channel~Well", scales="free_y")
-    + gg.theme_bw()
-    + gg.theme(strip_background=gg.element_rect(colour="black", fill="#fdfff4"))
+gg.ggplot(PLLS_df, gg.aes(x="Site", y="value", label = "Site")) +
+gg.geom_point() +
+gg.coord_fixed(ratio=.25) +
+gg.geom_text(nudge_y = -.025) +
+gg.facet_grid("Channel~Well", scales = "free_y") +
+gg.theme_bw() +
+gg.theme(strip_background=gg.element_rect(colour="black", fill="#fdfff4"))
 )
 
 output_file = pathlib.Path(output_figuresdir, "PLLS_per_well.png")
@@ -294,17 +251,15 @@ for name in painting_image_names:
     cp_sat_cols.append("ImageQuality_PercentMaximal_" + name)
 for x in range(1, (barcoding_cycles + 1)):
     for nt in nts:
-        bc_sat_cols.append(
-            "ImageQuality_PercentMaximal_" + barcoding_prefix + "%02d" % x + "_" + nt
-        )
+        bc_sat_cols.append("ImageQuality_PercentMaximal_" + barcoding_prefix + "%02d" % x + "_" + nt)
 for col in cp_sat_cols:
     cp_sat_df = image_df[image_df[col] > 1]
 for col in bc_sat_cols:
-    bc_sat_df = image_df[image_df[col] > 0.25]
+    bc_sat_df = image_df[image_df[col] > .25]
 sat_df_cols = cp_sat_cols + bc_sat_cols
 sat_df_cols.append("site")
 
-sat_df = cp_sat_df.append(bc_sat_df).drop_duplicates(subset="site")
+sat_df = cp_sat_df.append(bc_sat_df).drop_duplicates(subset = "site")
 
 if len(sat_df.index) > 0:
     sat_output_file = pathlib.Path(results_output, "saturated_sites.csv")
@@ -317,22 +272,20 @@ for name in painting_image_names:
     cp_sat_df_cols.append("ImageQuality_StdIntensity_" + name)
 cp_sat_df = image_df.loc[:, cp_sat_df_cols]
 
-cp_sat_df = cp_sat_df.set_index(["Plate", "Well", "Site"]).stack().reset_index()
-cp_sat_df[["cat", "type", "Ch"]] = cp_sat_df["level_3"].str.split("_", n=2, expand=True)
-cp_sat_df = cp_sat_df.drop(["level_3", "cat"], 1)
-cp_sat_df = pd.pivot_table(
-    cp_sat_df, index=["Plate", "Well", "Site", "Ch"], columns=["type"]
-).reset_index()
-cp_sat_df.columns = ["Plate", "Well", "Site", "Ch", "PercentMax", "StdIntensity"]
+cp_sat_df = cp_sat_df.set_index(['Plate', 'Well', 'Site']).stack().reset_index()
+cp_sat_df[['cat', 'type', 'Ch']] = cp_sat_df['level_3'].str.split('_', n=2, expand=True)
+cp_sat_df = cp_sat_df.drop(['level_3', 'cat'], 1)
+cp_sat_df = pd.pivot_table(cp_sat_df, index=['Plate', 'Well', 'Site', 'Ch'], columns=['type']).reset_index()
+cp_sat_df.columns = ['Plate', 'Well', 'Site', 'Ch', 'PercentMax', 'StdIntensity']
 
 cp_saturation_gg = (
-    gg.ggplot(cp_sat_df, gg.aes(x="StdIntensity", y="PercentMax", label="Site"))
-    + gg.geom_point()
-    + gg.coord_fixed(ratio=0.25)
-    + gg.geom_text()
-    + gg.facet_wrap(["Ch", "Well"], nrow=len(painting_image_names), scales="free")
-    + gg.theme_bw()
-    + gg.theme(strip_background=gg.element_rect(colour="black", fill="#fdfff4"))
+gg.ggplot(cp_sat_df, gg.aes(x="StdIntensity", y="PercentMax", label = "Site")) +
+gg.geom_point() +
+gg.coord_fixed(ratio=.25) +
+gg.geom_text() +
+gg.facet_wrap(["Ch", "Well"], nrow = len(painting_image_names), scales = "free") +
+gg.theme_bw() +
+gg.theme(strip_background=gg.element_rect(colour="black", fill="#fdfff4"))
 )
 output_file = pathlib.Path(output_figuresdir, "cp_saturation.png")
 cp_saturation_gg.save(output_file, dpi=300, verbose=False)
@@ -341,30 +294,24 @@ cp_saturation_gg.save(output_file, dpi=300, verbose=False)
 bc_sat_df_cols = ["Plate", "Well", "Site"]
 for x in range(1, (barcoding_cycles + 1)):
     for nt in nts:
-        bc_sat_df_cols.append(
-            "ImageQuality_PercentMaximal_" + barcoding_prefix + "%02d" % x + "_" + nt
-        )
-        bc_sat_df_cols.append(
-            "ImageQuality_StdIntensity_" + barcoding_prefix + "%02d" % x + "_" + nt
-        )
+        bc_sat_df_cols.append("ImageQuality_PercentMaximal_" + barcoding_prefix + "%02d" % x + "_" + nt)
+        bc_sat_df_cols.append("ImageQuality_StdIntensity_" + barcoding_prefix + "%02d" % x + "_" + nt)
 bc_sat_df = image_df.loc[:, bc_sat_df_cols]
 
-bc_sat_df = bc_sat_df.set_index(["Plate", "Well", "Site"]).stack().reset_index()
-bc_sat_df[["cat", "type", "Ch"]] = bc_sat_df["level_3"].str.split("_", n=2, expand=True)
-bc_sat_df = bc_sat_df.drop(["level_3", "cat"], 1)
-bc_sat_df = pd.pivot_table(
-    bc_sat_df, index=["Plate", "Well", "Site", "Ch"], columns=["type"]
-).reset_index()
-bc_sat_df.columns = ["Plate", "Well", "Site", "Ch", "PercentMax", "StdIntensity"]
+bc_sat_df = bc_sat_df.set_index(['Plate', 'Well', 'Site']).stack().reset_index()
+bc_sat_df[['cat', 'type', 'Ch']] = bc_sat_df['level_3'].str.split('_', n=2, expand=True)
+bc_sat_df = bc_sat_df.drop(['level_3', 'cat'], 1)
+bc_sat_df = pd.pivot_table(bc_sat_df, index=['Plate', 'Well', 'Site', 'Ch'], columns=['type']).reset_index()
+bc_sat_df.columns = ['Plate', 'Well', 'Site', 'Ch', 'PercentMax', 'StdIntensity']
 
 bc_saturation_gg = (
-    gg.ggplot(bc_sat_df, gg.aes(x="StdIntensity", y="PercentMax", label="Site"))
-    + gg.geom_point()
-    + gg.coord_fixed(ratio=0.25)
-    + gg.geom_text()
-    + gg.facet_wrap(["Ch", "Well"], ncol=4, scales="free")
-    + gg.theme_bw()
-    + gg.theme(strip_background=gg.element_rect(colour="black", fill="#fdfff4"))
+gg.ggplot(bc_sat_df, gg.aes(x="StdIntensity", y="PercentMax", label = "Site")) +
+gg.geom_point() +
+gg.coord_fixed(ratio=.25) +
+gg.geom_text() +
+gg.facet_wrap(["Ch", "Well"], ncol = 4, scales = "free") +
+gg.theme_bw() +
+gg.theme(strip_background=gg.element_rect(colour="black", fill="#fdfff4"))
 )
 output_file = pathlib.Path(output_figuresdir, "bc_saturation.png")
 bc_saturation_gg.save(output_file, dpi=300, verbose=False)
