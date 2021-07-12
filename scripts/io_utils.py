e3a828ecbddecc7703e7a05c0e689b4775576332
bethac07 <bcimini@broadinstitute.org>
2021-07-12 13:43:58 -0400

    use context manager

--- scripts/io_utils.py
+++ scripts/io_utils.py
@@ -1,6 +1,8 @@
 import pathlib
 import warnings
 
+import pandas as pd
+
 
 def check_if_write(file_path, force, throw_warning=False):
     if file_path.exists():
@@ -23,10 +25,11 @@ def read_csvs_with_chunksize(filename, chunksize=10000, **kwargs):
     can continue to be handled how they currently are in the various locations.
     """
     try:
-        dflist = []
-        for chunk in pd.read_csv(filename, chunksize=chunksize, **kwargs):
-            dflist.append(chunk)
-        df = pd.concat(dflist)
-        return df
+        with pd.read_csv(filename, chunksize=chunksize, **kwargs) as reader:
+            dflist = []
+            for chunk in reader:
+                dflist.append(chunk)
+            df = pd.concat(dflist)
+            return df
     except:
         raise
