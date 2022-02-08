f8a1fb08b168be9ee2337b8461072b9f074ff82f
ErinWeisbart <eweisbar@broadinstitute.org>
2022-02-08 10:04:15 -0800

    parallelize single_cell_by_guide creation

--- 1.generate-profiles/2.normalize.py
+++ 1.generate-profiles/2.normalize.py
@@ -6,6 +6,7 @@ import warnings
 import logging
 import traceback
 import pandas as pd
+from joblib import Parallel, delayed
 
 from pycytominer import normalize
 
@@ -91,6 +92,25 @@ site_info_dict = get_split_aware_site_info(
     config["experiment"], sites, split_info, separator="___"
 )
 
+def append_to_guide_csv(guide, image_df):
+    append_df = df.loc[
+        df["Metadata_Foci_Barcode_MatchedTo_Barcode"] == guide
+    ]
+    gene = list(append_df["Metadata_Foci_Barcode_MatchedTo_Barcode"])[0]
+    guide_file_name = f"{str(output_file).split('__')[0].split('/')[-1]}__{guide}_{gene}.csv.gz"
+    guide_path = os.path.join(sc_by_guide_folder, guide_file_name)
+    if not os.path.exists(guide_path):
+        guide_df = pd.DataFrame()
+    else:
+        guide_df = read_csvs_with_chunksize(guide_path)
+
+    append_df = append_df.merge(
+        image_df, left_on="Metadata_Foci_site", right_on="Metadata_site"
+    )
+    guide_df = guide_df.append(append_df)
+    guide_df.to_csv(guide_path, index=False)
+
+
 for data_split_site in site_info_dict:
     for data_level in normalize_levels:
         if data_level == "single_cell":
@@ -139,6 +159,7 @@ for data_split_site in site_info_dict:
                 logging.info(
                     f"Now outputting normalized single cell profiles by guide for split {data_split_site}"
                 )
+                # Load image alignment information for appending to single_cell_by_guide csvs
                 image_df = pd.read_csv(image_file, sep="\t")
                 keep_columns = []
                 for col in image_df.columns:
@@ -153,24 +174,7 @@ for data_split_site in site_info_dict:
                 if not os.path.isdir(sc_by_guide_folder):
                     os.mkdir(sc_by_guide_folder)
                 df = read_csvs_with_chunksize(output_file)
-                for guide in set(df["Metadata_Foci_Barcode_MatchedTo_Barcode"]):
-                    gene = df[df["Metadata_Foci_Barcode_MatchedTo_Barcode"] == guide][
-                        "Metadata_Foci_Barcode_MatchedTo_GeneCode"
-                    ].tolist()[0]
-                    guide_file_name = f"{str(output_file).split('__')[0].split('/')[-1]}__{guide}_{gene}.csv.gz"
-                    guide_path = os.path.join(sc_by_guide_folder, guide_file_name)
-                    if not os.path.exists(guide_path):
-                        guide_df = pd.DataFrame()
-                    else:
-                        guide_df = read_csvs_with_chunksize(guide_path)
-                    append_df = df.loc[
-                        df["Metadata_Foci_Barcode_MatchedTo_Barcode"] == guide
-                    ]
-                    append_df = append_df.merge(
-                        image_df, left_on="Metadata_Foci_site", right_on="Metadata_site"
-                    )
-                    guide_df = guide_df.append(append_df)
-                    guide_df.to_csv(guide_path, index=False)
+                Parallel(n_jobs=-2)(delayed(append_to_guide_csv)(guide, image_df) for guide in set(df["Metadata_Foci_Barcode_MatchedTo_Barcode"]))
 
 print("Finished 2.normalize.")
 logging.info("Finished 2.normalize.")
